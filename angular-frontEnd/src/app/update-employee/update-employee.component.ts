66d2d035f5902352ce38babe8b50e122e478daf0
dhanushkagimhan <dhanushkagimhan@gmail.com>
2021-07-11 19:21:15 +0530

    created update ui

--- angular-frontEnd/src/app/update-employee/update-employee.component.ts
+++ angular-frontEnd/src/app/update-employee/update-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-update-employee',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateEmployeeComponent implements OnInit {
 
+  employee: Employee = new Employee();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    
   }
 
 }
